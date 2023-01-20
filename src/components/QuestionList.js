bd3ec236caf63ed48e924fdfa2160ec0ae1dc523
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-01-20 12:55:46 +0300

    completed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect ,useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions,setQuestions]=useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then(r=>r.json())
+    .then((questions)=>setQuestions(questions))
+},[])
+
+function handleDeleteItem(deletedItem) {
+ const updatedQuestions = questions.filter((question) => question.id !== deletedItem.id);
+  setQuestions(updatedQuestions);
+}
+function newQuestion(e){
+  e.preventDefault()
+  const newQuestion={
+    question:{
+      
+    }
+  }
+}
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      {questions.map((question)=>{
+       return  <QuestionItem key={question.id}  onDeleteItem={handleDeleteItem} question={question}/>
+      })/* display QuestionItem components here after fetching */}
     </section>
   );
 }
